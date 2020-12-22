65aa805a9d56dc401760deca357a00fe3ff85035
jaylehri <jnlehri@bestpeers.com>
2020-12-22 10:53:05 +0530

    status change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,24 +13,17 @@ class OrdersController < ApplicationController
     @total_quantity = LineItem.where(user_id: current_user.id).sum('quantity')
     @total_price =  LineItem.where(user_id: current_user.id).sum('price')
     current_user.latest_order.update(total_price: @total_price, 
-      status: 'place',total_quantity:@total_quantity, payment_mode: @order.payment_mode)
+      status: 'payment',total_quantity:@total_quantity, payment_mode: @order.payment_mode)
     redirect_to orders_path
  end
  
-  def total_revanue
-      @sold_product_total_quantity = Order.sum('total_quantity')
-      @sold_product_total_price =  Order.sum('total_price')
+ def total_revanue
+    @sold_product_total_quantity = Order.sum('total_quantity')
+    @sold_product_total_price =  Order.sum('total_price')
   end
 
 
   private
-
-    def update_order 
-      @total_quantity = LineItem.where(user_id: current_user.id).sum('quantity')
-      @total_price =  LineItem.where(user_id: current_user.id).sum('price')
-      current_user.latest_order.update(total_price: @total_price, 
-      status: 'place',total_quantity:@total_quantity, payment_mode: @order.payment_mode)
-    end
 
     def order_params
       params.require(:order).permit(:total_price, :total_quantity, :status, :payment_mode, :user_id)
