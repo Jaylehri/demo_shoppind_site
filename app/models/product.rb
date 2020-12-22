03c10b8e5742ca8426870d5082f7ba78fed829d2
jaylehri <jnlehri@bestpeers.com>
2020-12-22 10:56:34 +0530

    make relation between user and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
  # before_destroy :not_referenced_by_any_line_item
   has_many_attached :images, dependent: :destroy
   has_many :line_items, dependent: :destroy 
+  belongs_to :user
 end
